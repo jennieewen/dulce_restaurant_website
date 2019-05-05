8e332a1a5cdc938ffb7dfef258d80f2f57d1b84f
Jennifer Wen <jenniferwen84@gmail.com>
2019-05-05 21:18:08 +0800

    Update settings.py

--- onyx/settings.py
+++ onyx/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tb^ng^#*d)n)zn97i@58h!%zyxflek#7zy1do(h27xra*r59-%'
+SECRET_KEY = 'n97i@5sihdpasdhpaasday1h27xra*r59-%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
